a549e4af49c290bbd6f68686d48766f861f311bc
Timothy Stotts <timothystotts08@gmail.com>
2020-06-27 21:50:33 -0400

    The led_pwm_driver was updated with staging of calculations to infer a single DSP48E1 per LED filament; a total of 16 DSP units for the Arty-A7-100T. Synthesis warnings are no longer generated. Additionally, the loop for the basic LEDs used the blueidx instead of basicidx, which was a copy-paste error.

--- ACL-Tester-Design-Single-Clock-Verilog/RTL/led_pwm_driver.v
+++ ACL-Tester-Design-Single-Clock-Verilog/RTL/led_pwm_driver.v
@@ -70,12 +70,9 @@ generate
 	genvar redidx;
 	for (redidx = 0; redidx < parm_color_led_count; redidx = redidx + 1) begin: redloop
 		integer s_color_red_pwm_period_count;
-		integer s_pwm_color_max_duty_cycle_1;
-		integer s_pwm_color_max_duty_cycle_2;
 		integer s_color_red_pwm_duty_cycles;
 		integer s_color_red_pwm_duty_cycles_1;
 		integer s_color_red_pwm_duty_cycles_2;
-		integer s_color_red_pwm_duty_cycles_3;
         reg [7:0] s_color_led_red_value_0;
         
 		always @(posedge i_clk)
@@ -84,6 +81,10 @@ generate
 				eo_color_leds_r[redidx] <= c_filament_off_value;
 				s_color_red_pwm_period_count <= c_pwm_period_ms - 1;
 				s_color_red_pwm_duty_cycles <= 0;
+				s_color_led_red_value_0 <= 8'h00;
+				s_color_red_pwm_duty_cycles_1 <= 0;
+				s_color_red_pwm_duty_cycles_2 <= 0;
+
 			end else begin
 				if (s_color_red_pwm_period_count > 0) begin
 					if (s_color_red_pwm_period_count < s_color_red_pwm_duty_cycles)
@@ -97,22 +98,18 @@ generate
 					s_color_red_pwm_period_count <= c_pwm_period_ms - 1;
 					
 					// Register the DSP48E1 output
-					s_color_red_pwm_duty_cycles <= s_color_red_pwm_duty_cycles_3;
+					s_color_red_pwm_duty_cycles <= s_color_red_pwm_duty_cycles_2;
 				end
-				
-				// Register inferred DSP48E1 Register A or D
-				s_pwm_color_max_duty_cycle_1 <= c_pwm_color_max_duty_cycle_ratioed;
-				s_pwm_color_max_duty_cycle_2 <= s_pwm_color_max_duty_cycle_1 + 0;
 				
 				// Register inferred DSP48E1 Register B
 				s_color_led_red_value_0 <= i_color_led_red_value[(redidx * 8 + 7)-:8];
 				
-				// Register inferred DSP48E1: Multiply
-				s_color_red_pwm_duty_cycles_1 <= s_pwm_color_max_duty_cycle_2 * s_color_led_red_value_0;
+				// Register inferred DSP48E1 Register A or D
+				// Register inferred DSP48E1 Multiply
+				s_color_red_pwm_duty_cycles_1 <= c_pwm_color_max_duty_cycle_ratioed * s_color_led_red_value_0;
 				
-				// Register the output P
+				// Register the inferred DSP48E1 output P
 				s_color_red_pwm_duty_cycles_2 <= s_color_red_pwm_duty_cycles_1;
-				s_color_red_pwm_duty_cycles_3 <= s_color_red_pwm_duty_cycles_2;
 			end
 		end
 	end
@@ -122,12 +119,9 @@ generate
 	genvar greenidx;
 	for (greenidx = 0; greenidx < parm_color_led_count; greenidx = greenidx + 1) begin: greenloop
 		integer s_color_green_pwm_period_count;
-		integer s_pwm_color_max_duty_cycle_1;
-		integer s_pwm_color_max_duty_cycle_2;
 		integer s_color_green_pwm_duty_cycles;
 		integer s_color_green_pwm_duty_cycles_1;
 		integer s_color_green_pwm_duty_cycles_2;
-		integer s_color_green_pwm_duty_cycles_3;
         reg [7:0] s_color_led_green_value_0;
         
 		always @(posedge i_clk)
@@ -136,6 +130,10 @@ generate
 				eo_color_leds_g[greenidx] <= c_filament_off_value;
 				s_color_green_pwm_period_count <= c_pwm_period_ms - 1;
 				s_color_green_pwm_duty_cycles <= 0;
+				s_color_led_green_value_0 <= 8'h00;
+				s_color_green_pwm_duty_cycles_1 <= 0;
+				s_color_green_pwm_duty_cycles_2 <= 0;
+
 			end else begin
 				if (s_color_green_pwm_period_count > 0) begin
 					if (s_color_green_pwm_period_count < s_color_green_pwm_duty_cycles)
@@ -149,22 +147,18 @@ generate
 					s_color_green_pwm_period_count <= c_pwm_period_ms - 1;
 					
 					// Register the DSP48E1 output
-					s_color_green_pwm_duty_cycles <= s_color_green_pwm_duty_cycles_3;
+					s_color_green_pwm_duty_cycles <= s_color_green_pwm_duty_cycles_2;
 				end
-				
-				// Register inferred DSP48E1 Register A or D
-				s_pwm_color_max_duty_cycle_1 <= c_pwm_color_max_duty_cycle_ratioed;
-				s_pwm_color_max_duty_cycle_2 <= s_pwm_color_max_duty_cycle_1 + 0;
 				
 				// Register inferred DSP48E1 Register B
 				s_color_led_green_value_0 <= i_color_led_green_value[(greenidx * 8 + 7)-:8];
 				
-				// Register inferred DSP48E1: Multiply
-				s_color_green_pwm_duty_cycles_1 <= s_pwm_color_max_duty_cycle_2 * s_color_led_green_value_0;
+				// Register inferred DSP48E1 Register A or D
+				// Register inferred DSP48E1 Multiply
+				s_color_green_pwm_duty_cycles_1 <= c_pwm_color_max_duty_cycle_ratioed * s_color_led_green_value_0;
 				
-				// Register the output P
+				// Register the inferred DSP48E1 output P
 				s_color_green_pwm_duty_cycles_2 <= s_color_green_pwm_duty_cycles_1;
-				s_color_green_pwm_duty_cycles_3 <= s_color_green_pwm_duty_cycles_2;
 			end
 		end
 	end
@@ -174,12 +168,9 @@ generate
 	genvar blueidx;
 	for (blueidx = 0; blueidx < parm_color_led_count; blueidx = blueidx + 1) begin: blueloop
 		integer s_color_blue_pwm_period_count;
-		integer s_pwm_color_max_duty_cycle_1;
-		integer s_pwm_color_max_duty_cycle_2;
 		integer s_color_blue_pwm_duty_cycles;
 		integer s_color_blue_pwm_duty_cycles_1;
 		integer s_color_blue_pwm_duty_cycles_2;
-		integer s_color_blue_pwm_duty_cycles_3;
         reg [7:0] s_color_led_blue_value_0;
         
 		always @(posedge i_clk)
@@ -188,6 +179,10 @@ generate
 				eo_color_leds_b[blueidx] <= c_filament_off_value;
 				s_color_blue_pwm_period_count <= c_pwm_period_ms - 1;
 				s_color_blue_pwm_duty_cycles <= 0;
+				s_color_led_blue_value_0 <= 8'h00;
+				s_color_blue_pwm_duty_cycles_1 <= 0;
+				s_color_blue_pwm_duty_cycles_2 <= 0;
+
 			end else begin
 				if (s_color_blue_pwm_period_count > 0) begin
 					if (s_color_blue_pwm_period_count < s_color_blue_pwm_duty_cycles)
@@ -201,22 +196,18 @@ generate
 					s_color_blue_pwm_period_count <= c_pwm_period_ms - 1;
 					
 					// Register the DSP48E1 output
-					s_color_blue_pwm_duty_cycles <= s_color_blue_pwm_duty_cycles_3;
+					s_color_blue_pwm_duty_cycles <= s_color_blue_pwm_duty_cycles_2;
 				end
-				
-				// Register inferred DSP48E1 Register A or D
-				s_pwm_color_max_duty_cycle_1 <= c_pwm_color_max_duty_cycle_ratioed;
-				s_pwm_color_max_duty_cycle_2 <= s_pwm_color_max_duty_cycle_1 + 0;
 				
 				// Register inferred DSP48E1 Register B
 				s_color_led_blue_value_0 <= i_color_led_blue_value[(blueidx * 8 + 7)-:8];
 				
+				// Register inferred DSP48E1 Register A or D
 				// Register inferred DSP48E1: Multiply
-				s_color_blue_pwm_duty_cycles_1 <= s_pwm_color_max_duty_cycle_2 * s_color_led_blue_value_0;
+				s_color_blue_pwm_duty_cycles_1 <= c_pwm_color_max_duty_cycle_ratioed * s_color_led_blue_value_0;
 				
-				// Register the output P
+				// Register the inferred DSP48E1 output P
 				s_color_blue_pwm_duty_cycles_2 <= s_color_blue_pwm_duty_cycles_1;
-				s_color_blue_pwm_duty_cycles_3 <= s_color_blue_pwm_duty_cycles_2;
 			end
 		end
 	end
@@ -226,49 +217,46 @@ generate
 	genvar basicidx;
 	for (basicidx = 0; basicidx < parm_basic_led_count; basicidx = basicidx + 1) begin: basicloop
 		integer s_basic_lumin_pwm_period_count;
-		integer s_pwm_basic_max_duty_cycle_1;
-		integer s_pwm_basic_max_duty_cycle_2;
 		integer s_basic_lumin_pwm_duty_cycles;
 		integer s_basic_lumin_pwm_duty_cycles_1;
 		integer s_basic_lumin_pwm_duty_cycles_2;
-		integer s_basic_lumin_pwm_duty_cycles_3;
         reg [7:0] s_basic_led_lumin_value_0;
         
 		always @(posedge i_clk)
 		begin: p_operate_basic_lumin_pwm
 			if (i_srst) begin
-				eo_basic_leds_l[blueidx] <= c_filament_off_value;
+				eo_basic_leds_l[basicidx] <= c_filament_off_value;
 				s_basic_lumin_pwm_period_count <= c_pwm_period_ms - 1;
 				s_basic_lumin_pwm_duty_cycles <= 0;
+				s_basic_led_lumin_value_0 <= 8'h00;
+				s_basic_lumin_pwm_duty_cycles_1 <= 0;
+				s_basic_lumin_pwm_duty_cycles_2 <= 0;
+
 			end else begin
 				if (s_basic_lumin_pwm_period_count > 0) begin
 					if (s_basic_lumin_pwm_period_count < s_basic_lumin_pwm_duty_cycles)
-						eo_basic_leds_l[blueidx] <= c_filament_on_value;
+						eo_basic_leds_l[basicidx] <= c_filament_on_value;
 					else
-						eo_basic_leds_l[blueidx] <= c_filament_off_value;
+						eo_basic_leds_l[basicidx] <= c_filament_off_value;
 				
 					s_basic_lumin_pwm_period_count <= s_basic_lumin_pwm_period_count - 1;
 				end else begin
-					eo_basic_leds_l[blueidx] <= c_filament_on_value;
+					eo_basic_leds_l[basicidx] <= c_filament_on_value;
 					s_basic_lumin_pwm_period_count <= c_pwm_period_ms - 1;
 					
 					// Register the DSP48E1 output
-					s_basic_lumin_pwm_duty_cycles <= s_basic_lumin_pwm_duty_cycles_3;
+					s_basic_lumin_pwm_duty_cycles <= s_basic_lumin_pwm_duty_cycles_2;
 				end
-				
-				// Register inferred DSP48E1 Register A or D
-				s_pwm_basic_max_duty_cycle_1 <= c_pwm_basic_max_duty_cycle_ratioed;
-				s_pwm_basic_max_duty_cycle_2 <= s_pwm_basic_max_duty_cycle_1 + 0;
 				
 				// Register inferred DSP48E1 Register B
 				s_basic_led_lumin_value_0 <= i_basic_led_lumin_value[(basicidx * 8 + 7)-:8];
 				
-				// Register inferred DSP48E1: Multiply
-				s_basic_lumin_pwm_duty_cycles_1 <= s_pwm_basic_max_duty_cycle_2 * s_basic_led_lumin_value_0;
+				// Register inferred DSP48E1 Register A or D
+				// Register inferred DSP48E1 Multiply
+				s_basic_lumin_pwm_duty_cycles_1 <= c_pwm_basic_max_duty_cycle_ratioed * s_basic_led_lumin_value_0;
 				
 				// Register the output P
 				s_basic_lumin_pwm_duty_cycles_2 <= s_basic_lumin_pwm_duty_cycles_1;
-				s_basic_lumin_pwm_duty_cycles_3 <= s_basic_lumin_pwm_duty_cycles_2;
 			end
 		end
 	end
