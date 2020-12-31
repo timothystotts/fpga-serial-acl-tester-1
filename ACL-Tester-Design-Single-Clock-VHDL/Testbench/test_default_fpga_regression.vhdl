42463a1fb9b8241f3b3a8261dff6e37d04fafc9b
Timothy Stotts <timothystotts08@gmail.com>
2020-12-31 14:07:11 -0500

    The VHDL configuration test_default_fpga_regression was updated as template for additional tests.

--- ACL-Tester-Design-Single-Clock-VHDL/Testbench/test_default_fpga_regression.vhdl
+++ ACL-Tester-Design-Single-Clock-VHDL/Testbench/test_default_fpga_regression.vhdl
@@ -20,6 +20,36 @@ configuration test_default_fpga_regression of fpga_serial_acl_tester_testharness
             generic map(
                 parm_log_file_name => "log_test_default_fpga_regression.txt"
             );
+
+            for simulation
+                for uut_fpga_serial_acl_tester : fpga_serial_acl_tester
+                    use entity work.fpga_serial_acl_tester(rtl);
+                end for;
+
+                for u_tbc_clock_gen : tbc_clock_gen
+                    use entity work.tbc_clock_gen(simulation_default);
+                end for;
+
+                for u_tbc_board_ui : tbc_board_ui
+                    use entity work.tbc_board_ui(simulation_default);
+                end for;
+
+                for u_tbc_pmod_acl2 : tbc_pmod_acl2
+                    use entity work.tbc_pmod_acl2(simulation_default);
+                end for;
+
+                for u_tbc_pmod_cls : tbc_pmod_cls
+                    use entity work.tbc_pmod_cls(simulation_default);
+                end for;
+
+                for u_tbc_board_uart : tbc_board_uart
+                    use entity work.tbc_board_uart(simulation_default);
+                end for;
+
+                for u_tbc_pmod_7sd : tbc_pmod_7sd
+                    use entity work.tbc_pmod_7sd(simulation_default);
+                end for;
+            end for;
         end for;
     end for;
 end configuration test_default_fpga_regression;
