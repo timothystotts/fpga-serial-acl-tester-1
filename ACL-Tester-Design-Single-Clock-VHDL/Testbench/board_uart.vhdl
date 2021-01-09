18e54c6f1dd72c9f4fdc029e1a5ca9ce66c8eb48
Timothy Stotts <timothystotts08@gmail.com>
2021-01-09 14:34:07 -0500

    Updated the tbc_board_uart with comments describing variables, signals, and process logic, including the ScoreBoard operation.

--- ACL-Tester-Design-Single-Clock-VHDL/Testbench/board_uart.vhdl
+++ ACL-Tester-Design-Single-Clock-VHDL/Testbench/board_uart.vhdl
@@ -51,9 +51,10 @@ entity tbc_board_uart is
 end entity tbc_board_uart;
 --------------------------------------------------------------------------------
 architecture simulation_default of tbc_board_uart is
+    -- Simulation logging ID for this architecture.
 	signal ModelID : AlertLogIDType;
 begin
-	-- Simulation initialization
+	-- Simulation initialization for the tbc_board_uart component.
 	p_sim_init : process
 		variable ID : AlertLogIDType;
 	begin
@@ -70,43 +71,88 @@ begin
 	end process p_sim_init;
 
 	UartTbRxProc : process
-		variable RxStim       : UartStimType;
+		-- A variable to assign ModelID and use with procedures that can only
+		-- accept a variable instead of a signal for the Alert/Log ID.
 		variable ID           : AlertLogIDType;
+		-- Record to track UART RX communication.
+		variable RxStim       : UartStimType;
+		-- The ASCII line buffer.
 		variable rx_ascii_buf : string(1 to 64) := (others => NUL);
+		-- The ASCII line buffer position/length count.
 		variable rx_ascii_cnt : natural         := 0;
+		-- The current ASCII character after ASCII table conversion.
 		variable rx_ascii_chr : character;
+		-- The flag to track if the ASCII buffer overflowed during this
+		-- iteration of capturing RX data.
 		variable rx_ascii_ovr : boolean := false;
+		-- The byte line buffer.
 		variable v_hex_expect : std_logic_vector(255 downto 0);
+		-- The byte line buffer position/length count.
 		variable v_hex_cnt    : natural := 0;
-		variable v_slv_expect : std_logic_vector(63 downto 0);
-		constant c_all_undef : std_logic_vector(63 downto 0) := (others => 'U');
+		-- The ScoreBoard binary value for finding in the history.
+		variable v_slv_expect : t_reg_sb;
+		-- A constant that is all Undefind, same type as the \ref v_slv_expect .
+		constant c_all_undef : t_reg_sb := (others => 'U');
+		-- Variable for querying the ScoreBoard FIFO position of the matching
+		-- data \ref v_slv_expect .
 		variable v_expect_idx : integer;
 	begin
+		wait for 0 ns;
 		WaitForBarrier(BarrierLogStart);
 		ID := ModelID;
 		GetAlertLogID(TransRec, ID);
 		WaitForClock(TransRec, 2);
 
-		-- Get with one parameter
+		-- Loop receiving one byte at a time via RX UART in the test-bench.
+		-- Upon receiving '\n' Line Feed character, analyze the received
+		-- vector of bytes, pressuming that they are ASCII bytes, and that
+		-- the vector contains a representation of the ScoreBoard type with
+		-- 16 hexadecimal nibbles.
 		l_recv_uart : loop
+			-- Receive the next RX byte
 			Get(TransRec, RxStim.Data);
+			-- Prepare to track any errors
 			RxStim.Error := std_logic_vector(TransRec.ParamFromModel);
 
 			if (RxStim.Data = x"0A") then
-				AlertIf(ModelID, rx_ascii_ovr, "The test model overflowed receiving UART ASCII text line. Not all characters are displayed.", ERROR);
-				Log(ModelID, "UART Received from FPGA the ASCII line: " & rx_ascii_buf(1 to rx_ascii_cnt), INFO);
+				-- If the received byte is '\n' Line Feed, attempt to convert
+				-- and find in the ScoreBoard.
 
+				-- If the ASCII line buffer overflowed, alert an error.
+				AlertIf(ModelID, rx_ascii_ovr, "The test model overflowed " &
+					"receiving UART ASCII text line. Not all characters are " &
+					"displayed.",
+					ERROR);
+				-- Log the ASCII line received
+				Log(ModelID, "UART Received from FPGA the ASCII line: " &
+					rx_ascii_buf(1 to rx_ascii_cnt),
+					INFO);
+
+				-- Convert the ASCII line to 64-bits of hexadecimal data
 				v_slv_expect(63 downto 32) := fn_convert_hex_to_slv32(v_hex_expect(255 downto 128), 8);
 				v_slv_expect(31 downto 0) := fn_convert_hex_to_slv32(v_hex_expect(127 downto 0), 8);
 
 				if (v_slv_expect /= c_all_undef) then
+					-- If the expected data is not all "U" bits, then attempt to
+					-- find the expected vector in the ScoreBoard. It will
+					-- likely not be the first in the FIFO as the UART runs
+					-- at a less frequent interval that the Pmod ACL2 filter
+					-- rate. Thus, find the match and drop all preceding values.
 					v_expect_idx := SB_UART.Find(v_slv_expect);
 					Log(ModelID, "BOARD UART text line matched ScoreBoard history: " & to_string(v_expect_idx), INFO);
 					SB_UART.Flush(v_expect_idx);
 				else
-					Alert(ModelID, "BOARD UART text line not tested with ScoreBoard history.", WARNING);
+					-- If \ref v_slv_expect is all "U" bits, then this ASCII
+					-- line was something other than raw register values. Thus,
+					-- do not test it with the ScoreBoard, but alert with a
+					-- warning.
+					Alert(ModelID, "BOARD UART text line not tested with " &
+						"ScoreBoard history.",
+						WARNING);
 				end if;
 
+				-- Reset data tracking in preparation of receiving next text
+				-- line.
 				rx_ascii_buf := (others => NUL);
 				rx_ascii_cnt := 0;
 				rx_ascii_ovr := false;
@@ -114,20 +160,36 @@ begin
 				v_hex_cnt := 0;
 				v_slv_expect := (others => 'U');
 			else
+				-- Use a 8-bit at a time shift register to capture the current
+				-- raw ASCII value from the RX UART. Only capture if the number 
+				-- of bytes received this far is less than the length of the
+				-- buffer, which is 32 ASCII bytes, same as the Pmod CLS
+				-- display.
 				if (v_hex_cnt < v_hex_expect'length / 8) then
 					v_hex_expect := v_hex_expect((v_hex_expect'length - 1 - 8) downto 0) & RxStim.Data;
 					v_hex_cnt := v_hex_cnt + 1;
 				end if;
 
+				-- Log the captured byte, both in raw vector value as well as
+				-- the ASCII character conversion.
 				rx_ascii_chr := fn_convert_slv_to_ascii(RxStim.Data);
-				Log(ModelID, "UART Received ASCII byte: x" & to_hstring(RxStim.Data) &
-					" '" & rx_ascii_chr & "'", DEBUG);
+				Log(ModelID, "UART Received ASCII byte: x" &
+					to_hstring(RxStim.Data) &
+					" '" & rx_ascii_chr & "'",
+					DEBUG);
+
+				-- Store the current ASCII character conversion, but only if
+				-- the ASCII line buffer is not overflowed, and only if the
+				-- the ASCII conversion is not carriage return '\r' or line
+				-- feed '\n'.
 				if (rx_ascii_cnt <= rx_ascii_buf'right) then
 					if ((RxStim.Data /= x"0A") and (RxStim.Data /= x"0D")) then
 						rx_ascii_cnt := rx_ascii_cnt + 1;
 						rx_ascii_buf(rx_ascii_cnt) := rx_ascii_chr;
 					end if;
 				else
+					-- If the ASCII buffer overlowed, flag for the Alert error
+					-- when the ASCII line ends.
 					rx_ascii_ovr := true;
 				end if;
 			end if;
