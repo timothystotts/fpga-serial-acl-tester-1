b308025f3e961e54d71aea4a03b0133b23530333
Timothy Stotts <timothystotts08@gmail.com>
2021-01-10 15:07:39 -0500

    Comments were augmented in the Pmod ACL2 test-bench component.

--- ACL-Tester-Design-Single-Clock-VHDL/Testbench/pmod_acl2.vhdl
+++ ACL-Tester-Design-Single-Clock-VHDL/Testbench/pmod_acl2.vhdl
@@ -36,17 +36,61 @@ context osvvm.OsvvmContext;
 library work;
 --------------------------------------------------------------------------------
 package tbc_pmod_acl2_types_pkg is
+	-- Byte address of the ADXL362 status register. See ADI datasheet.
+	constant c_reg_idx_status_reg : natural := 16#0B#;
+	-- Byte address of the signed XDATA value, lower byte. See ADI datasheet.
+	constant c_reg_idx_xdata_l    : natural := 16#0E#;
+	-- Byte address of the signed XDATA value, upper byte. See ADI datasheet.
+	constant c_reg_idx_xdata_h    : natural := 16#0F#;
+	-- Byte address of the signed YDATA value, lower byte. See ADI datasheet.
+	constant c_reg_idx_ydata_l    : natural := 16#10#;
+	-- Byte address of the signed YDATA value, upper byte. See ADI datasheet.
+	constant c_reg_idx_ydata_h    : natural := 16#11#;
+	-- Byte address of the signed ZDATA value, lower byte. See ADI datasheet.
+	constant c_reg_idx_zdata_l    : natural := 16#12#;
+	-- Byte address of the signed ZDATA value, upper byte. See ADI datasheet.
+	constant c_reg_idx_zdata_h    : natural := 16#13#;
+	-- Byte address of the signed TEMP value, lower byte. See ADI datasheet.
+	constant c_reg_idx_temp_l     : natural := 16#14#;
+	-- Byte address of the signed TEMP value, upper byte. See ADI datasheet.
+	constant c_reg_idx_temp_h     : natural := 16#15#;
+	-- Byte address of the ADXL362 INT1 Mapping register. See ADI datasheet.
+	constant c_reg_idx_int1_map   : natural := 16#2A#;
+	-- Byte address of the ADXL362 INT2 Mapping register. See ADI datasheet.
+	constant c_reg_idx_int2_map   : natural := 16#2B#;
+	-- Byte address of the ADXL362 Filter Control register. See ADI datasheet.
+	constant c_reg_idx_filter_ctl : natural := 16#2C#;
+
+	-- Register description type: permissions: Read-Only, Write-Only,
+	-- Read-Write, Reserved.
 	type t_reg_perm is (REG_R, REG_W, REG_RW, REG_RESV);
+
+	-- Register value array type: integer byte for an integer address range.
+	-- Integers can be specified in base-16 with 16#value# .
 	type t_reg_array is array (natural range <>) of natural range 0 to 255;
+
+	-- Register permission array type: array of \ref t_reg_perm .
 	type t_reg_perms is array(natural range <>) of t_reg_perm;
+
+	-- Register address number type: a constrained integer. The ADI datasheet
+	-- specifies 0 to 46. This type ranges 0 to 47 for less complex
+	-- SPI address auto-increment.
 	subtype t_op_addr is natural range 0 to 47;
 
+	-- A protected complex type to implement a 8-bit logic vector register
+	-- with access to shared between multiple architecture processes. 
 	type t_share_reg is protected
 		impure function Get return std_logic_vector;
 		procedure Set(constant c_reg    : in std_logic_vector(7 downto 0));
 		procedure Update(constant c_bit :    std_logic; constant c_pos : natural range 0 to 7);
-		end protected t_share_reg;
+	end protected t_share_reg;
 
+	-- A protected complex type to implement a signed 16-bit logic vector
+	-- register with access to shared between multiple architecture processes.
+	-- This type also implements MAX and MIN boundaries, with increment and
+	-- decrement methods for the purpose of quickly modifying the register
+	-- value, presuming that the register contains a data reading and not
+	-- controls.
 	type t_share_s16bit_reg is protected
 		procedure SetMax(constant c_max : in std_logic_Vector(15 downto 0));
 		procedure SetMin(constant c_min : in std_logic_vector(15 downto 0));
@@ -57,7 +101,7 @@ package tbc_pmod_acl2_types_pkg is
 				variable capped : out boolean);
 		procedure Decrement_By(constant c_val : in natural range 0 to 2**16-1;
 				variable capped : out boolean);
-		end protected t_share_s16bit_reg;
+	end protected t_share_s16bit_reg;
 end package tbc_pmod_acl2_types_pkg;
 --------------------------------------------------------------------------------
 package body tbc_pmod_acl2_types_pkg is
@@ -153,19 +197,8 @@ library work;
 use work.tbc_pmod_acl2_types_pkg.all;
 --------------------------------------------------------------------------------
 package tbc_pmod_acl2_pkg is
-	constant c_reg_idx_status_reg : natural := 16#0B#;
-	constant c_reg_idx_int1_map   : natural := 16#2A#;
-	constant c_reg_idx_int2_map   : natural := 16#2B#;
-	constant c_reg_idx_filter_ctl : natural := 16#2C#;
-	constant c_reg_idx_xdata_l    : natural := 16#0E#;
-	constant c_reg_idx_xdata_h    : natural := 16#0F#;
-	constant c_reg_idx_ydata_l    : natural := 16#10#;
-	constant c_reg_idx_ydata_h    : natural := 16#11#;
-	constant c_reg_idx_zdata_l    : natural := 16#12#;
-	constant c_reg_idx_zdata_h    : natural := 16#13#;
-	constant c_reg_idx_temp_l     : natural := 16#14#;
-	constant c_reg_idx_temp_h     : natural := 16#15#;
-
+	-- A constant defining the default byte values of the full register map of
+	-- the ADXL362. See ADI datasheet.
 	constant c_acl2_reg_mem : t_reg_array(t_op_addr'low to t_op_addr'high) := (
 			16#AD#, 16#1D#, 16#F2#, 16#01#, 16#00#, 16#00#, 16#00#, 16#00#,
 			16#00#, 16#00#, 16#00#, 16#40#, 16#00#, 16#00#, 16#00#, 16#00#,
@@ -174,6 +207,9 @@ package tbc_pmod_acl2_pkg is
 			16#00#, 16#00#, 16#00#, 16#00#, 16#00#, 16#00#, 16#00#, 16#00#,
 			16#00#, 16#80#, 16#00#, 16#00#, 16#13#, 16#00#, 16#00#, 16#00#
 		);
+
+	-- A constant defining the register access types/permissions of the full
+	-- register map of the ADXL362. See datasheet.
 	constant c_acl2_reg_perms : t_reg_perms(t_op_addr'low to t_op_addr'high) := (
 			REG_R, REG_R, REG_R, REG_R, REG_RESV, REG_RESV, REG_RESV, REG_RESV,
 			REG_R, REG_R, REG_R, REG_R, REG_R, REG_R, REG_R, REG_R,
@@ -183,6 +219,8 @@ package tbc_pmod_acl2_pkg is
 			REG_RW, REG_RW, REG_RW, REG_RW, REG_RW, REG_RW, REG_RW, REG_RESV
 		);
 
+	-- A procedure to perform Mode 0 SPI register reading or writing, with
+	-- the Pmod ACL2 (containing ADXL362) as FPGA peripheral.
 	procedure pr_spi_reg_access_mode_zero(
 			constant ModelID       : in    AlertLogIDType;
 			signal sck             : in    std_logic;
@@ -228,41 +266,61 @@ package body tbc_pmod_acl2_pkg is
 		variable val_out_as_slv : std_logic_vector(7 downto 0);
 		variable val_in_as_int  : natural range 0 to 255;
 	begin
+		-- Set initial count and output values.
 		buffer_len := 0;
 		buffer_ovr := 0;
 		cipo       <= 'Z';
 
+		-- Upon entering this loop, it is presumed that outside the procedure
+		-- the process had already detected: wait until csn = '0'.
 		l_spi_recv : loop
+			-- Wait for a clock edge or deassertion of chip select
 			wait on sck, csn;
 
+			-- Upon falling edge of SPI clock
 			if (sck'event and sck = '0') then
 				if (op_read and (buffer_len <= 15)) then
+					-- High Z for first 16 bits, first bit without falling edge
+					-- and preceding the first rising edge of clock.
 					cipo <= 'Z';
 				elsif (op_write) then
+					-- For a write operation, continue to hold High Z.
 					cipo <= 'Z';
 				elsif (not(op_read or op_write)) then
+					-- For an unknown operation, continue to hold High Z.
 					cipo <= 'Z';
 				else
+					-- For a read operation, output the MSBit.
 					cipo <= out_buf(out_buf'left);
 				end if;
 
+				-- Shift the output buffer left by one bit, to discard the
+				-- MSBit of the buffer.
 				out_buf := out_buf(out_buf'left - 1 downto 1) & '0';
 			end if;
 
+			-- Upon rising edge of SPI clock
 			if (sck'event and sck = '1') then
+				-- Shift the input buffer left by one bit, to enqueue the
+				-- MSBbit of the buffer.
 				in_buf := in_buf(in_buf'left - 1 downto 1) & copi;
 
+				-- If input buffer not exceeded, count the bits received.
+				-- If maxed-out, then count the number of overrun bits.
 				if (buffer_len < in_buf'length) then
 					buffer_len := buffer_len + 1;
 				else
 					buffer_ovr := buffer_ovr + 1;
 				end if;
 
+				-- Test the first byte once received, to determine if the
+				-- operation mode is WRITE or READ or unknown.
 				if (buffer_len = 8) then
 					op_write := (in_buf(8 downto 1) = x"0A");
 					op_read  := (in_buf(8 downto 1) = x"0B");
 				end if;
 
+				-- Attempt to receive the Operation Address, an integer byte.
 				if (buffer_len = 16) then
 					if (op_write or op_read) then
 						op_addr_in := to_integer(unsigned(in_buf(8 downto 1)));
@@ -274,10 +332,21 @@ package body tbc_pmod_acl2_pkg is
 					end if;
 				end if;
 
+				-- Convert the address integer to a logic byte.
 				op_addr_slv    := std_logic_vector(to_unsigned(op_addr, 8));
-				val_out_as_slv := std_logic_vector(to_unsigned(reg_mem(op_addr), 8));
+				-- Read the register memory at the address location, and place
+				-- in this variable.
+				val_out_as_slv := std_logic_vector(
+					to_unsigned(reg_mem(op_addr), 8));
+				-- Convert the input byte (lowest 8 bits at any time) to an
+				-- integer representation.
 				val_in_as_int  := to_integer(unsigned(in_buf(8 downto 1)));
 
+				-- On the clock count immediately before the FPGA bus Controller
+				-- reads a byte's first bit from this ACL2 peripheral, set the
+				-- MSByte of the output buffer. This only occurs immediately
+				-- before the output of the 3rd, 4th, 5th, etc byte, honoring
+				-- address auto-increment.
 				if (buffer_len mod 8 = 0) then
 					if (op_addr < t_op_addr'high) then
 						if ((op_read) and (buffer_len >= 16)) then
@@ -288,6 +357,11 @@ package body tbc_pmod_acl2_pkg is
 					end if;
 				end if;
 
+				-- On the clock count immediately after the FPGA bus Controller
+				-- reads a byte's first bit from the ACL2 peripheral, Log the
+				-- the address and value being read, check register permissions,
+				-- and if permissions exist for reading, auto-increment the read
+				-- address.
 				if (buffer_len mod 8 = 1) then
 					if (op_addr < t_op_addr'high) then
 						if ((op_read) and (buffer_len > 16)) then
@@ -309,6 +383,11 @@ package body tbc_pmod_acl2_pkg is
 					end if;
 				end if;
 
+				-- On the clock count immediately after a full byte has been
+				-- written via SPI, check the register permissions, if
+				-- permitted commit that byte to register memory, and flag
+				-- the byte as Pending so that the outer process can take to
+				-- synchronize memory bytes instead of overwritten.
 				if (buffer_len mod 8 = 0) then
 					if (op_addr < t_op_addr'high) then
 						if ((op_write) and (buffer_len >= 24)) then
@@ -333,6 +412,7 @@ package body tbc_pmod_acl2_pkg is
 				end if;
 			end if;
 
+			-- If the chip select rises, then exit this procedure.
 			if (csn = '1') then
 				exit;
 			end if;
@@ -368,10 +448,15 @@ entity tbc_pmod_acl2 is
 end entity tbc_pmod_acl2;
 --------------------------------------------------------------------------------
 architecture simulation_default of tbc_pmod_acl2 is
+	-- Shared status register byte.
 	shared variable sv_status_reg : t_share_reg;
+	-- Shared 16-bit signed XDATA register.
 	shared variable sv_xdata_reg  : t_share_s16bit_reg;
+	-- Shared 16-bit signed YDATA register.
 	shared variable sv_ydata_reg  : t_share_s16bit_reg;
+	-- Shared 16-bit signed ZDATA register.
 	shared variable sv_zdata_reg  : t_share_s16bit_reg;
+	-- Shared 16-bit signed TEMP register.	
 	shared variable sv_temp_reg   : t_share_s16bit_reg;
 
 	signal ModelID : AlertLogIDType;
